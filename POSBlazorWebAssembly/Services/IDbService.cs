603abb975e155b9f0a29563deb5f26ac330c4c5a
Mg Chit <chitlunh@gmail.com>
2023-04-29 18:43:49 +0630

    -

--- POSBlazorWebAssembly/Services/IDbService.cs
+++ POSBlazorWebAssembly/Services/IDbService.cs
@@ -6,5 +6,13 @@ namespace POSBlazorWebAssembly.Services
     {
         Task<List<ProductCreationDataModel>> GetProductList();
         Task SetProduct(ProductCreationDataModel model);
+        Task<ProductCreationDataModel> GetProduct(Guid guid);
+        Task ProductUpdate(ProductCreationDataModel model);
+        Task DeleteProduct(Guid guid);
+        Task<List<ProductCategoryDataModel>> GetProductCategoryList();
+        Task SetProductCategory(ProductCategoryDataModel model);
+        Task<ProductCategoryDataModel> GetProductCategory(Guid guid);
+        Task ProductCategoryUpdate(ProductCategoryDataModel model);
+        Task DeleteProductCategory(Guid guid);
     }
 }
--- POSBlazorWebAssembly/Services/LocalStorageService.cs
+++ POSBlazorWebAssembly/Services/LocalStorageService.cs
@@ -26,5 +26,89 @@ namespace POSBlazorWebAssembly.Services
             lst.Add(model);
             await localStorage.SetItemAsync("Tbl_Product", lst);
         }
+
+        public async Task<ProductCreationDataModel> GetProduct(Guid guid)
+        {
+            var lst = await localStorage.GetItemAsync<List<ProductCreationDataModel>>("Tbl_Product");
+            lst ??= new();
+            return lst.FirstOrDefault(x => x.product_id == guid);
+        }
+
+        public async Task ProductUpdate(ProductCreationDataModel model)
+        {
+            //var lst = await GetProduct(model.product_id);
+            var lst = await GetProductList();
+            lst ??= new();
+            var result = lst.FirstOrDefault(x => x.product_id == model.product_id);
+            int index = lst.FindIndex(x=> x.product_id == result.product_id);
+            result.product_id = model.product_id;
+            result.product_name = model.product_name;
+            result.product_code = model.product_code;
+            result.product_category_code = model.product_category_code;
+            result.product_buying_price = model.product_buying_price;
+            result.product_sale_price = model.product_sale_price;
+            result.product_cration_date = model.product_cration_date;
+            //lst.Add(result);
+            lst[index] = result;
+            await localStorage.SetItemAsync("Tbl_Product", lst);
+        }
+
+        public async Task DeleteProduct(Guid guid)
+        {
+            var lst = await GetProductList();
+            var item = lst.FirstOrDefault(x => x.product_id == guid);
+            if (item == null) return;
+            lst.Remove(item);
+            await localStorage.SetItemAsync("Tbl_Product",lst);
+        }
+
+        public async Task<List<ProductCategoryDataModel>> GetProductCategoryList()
+        {
+            List<ProductCategoryDataModel> lst = await localStorage.GetItemAsync<List<ProductCategoryDataModel>>("Tbl_ProductCategory");
+            lst ??= new();
+            return lst.OrderByDescending(x => x.product_category_creation_date).ToList();
+        }
+
+        public async Task SetProductCategory(ProductCategoryDataModel model)
+        {
+            List<ProductCategoryDataModel> lst = await localStorage.GetItemAsync<List<ProductCategoryDataModel>>("Tbl_ProductCategory");
+            lst ??= new();
+            lst.Add(model);
+            await localStorage.SetItemAsync("Tbl_ProductCategory", lst);
+        }
+
+        public async Task<ProductCategoryDataModel> GetProductCategory(Guid guid)
+        {
+            var lst = await localStorage.GetItemAsync<List<ProductCategoryDataModel>>("Tbl_ProductCategory");
+            lst ??= new();
+            return lst.FirstOrDefault(x => x.product_category_id == guid);
+        }
+
+        public async Task ProductCategoryUpdate(ProductCategoryDataModel model)
+        {
+            //var lst = await GetProduct(model.product_id);
+            var lst = await GetProductCategoryList();
+            lst ??= new();
+            var result = lst.FirstOrDefault(x => x.product_category_id == model.product_category_id);
+            int index = lst.FindIndex(x => x.product_category_id == result.product_category_id);
+            result.product_category_id = model.product_category_id;
+            result.product_category_name = model.product_category_name;
+            result.product_category_code = model.product_category_code;
+            result.product_category_creation_date = model.product_category_creation_date;
+            //lst.Add(result);
+            lst[index] = result;
+            await localStorage.SetItemAsync("Tbl_ProductCategory", lst);
+        }
+
+        public async Task DeleteProductCategory(Guid guid)
+        {
+            var lst = await GetProductCategoryList();
+            var item = lst.FirstOrDefault(x => x.product_category_id == guid);
+            if (item == null) return;
+            lst.Remove(item);
+            await localStorage.SetItemAsync("Tbl_ProductCategory", lst);
+        }
+
+
     }
 }
