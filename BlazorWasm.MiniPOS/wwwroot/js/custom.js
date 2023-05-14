dd2ff9732746542bc1356ee5279830f66bde5f80
Sann Lynn Htun <sannlynnhtun.error@gmail.com>
2023-05-15 04:53:30 +0630

    Apex PieChart - Completed

---
+++ BlazorWasm.MiniPOS/wwwroot/js/custom.js
@@ -0,0 +1,93 @@
+﻿window.bindPieChart = function (id, labels, series){
+    var options = {
+        series: series,
+        chart: {
+            width: 380,
+            type: 'pie',
+        },
+        labels: labels,
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    };
+
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+}
+
+window.bindPieChart2 = function (id, data) {
+    console.log({id, data});
+    // data = [{
+    //     name: 'Chrome',
+    //     y: 70.67,
+    //     sliced: true,
+    //     selected: true
+    // }, {
+    //     name: 'Edge',
+    //     y: 14.77
+    // }, {
+    //     name: 'Firefox',
+    //     y: 4.86
+    // }, {
+    //     name: 'Safari',
+    //     y: 2.63
+    // }, {
+    //     name: 'Internet Explorer',
+    //     y: 1.53
+    // }, {
+    //     name: 'Opera',
+    //     y: 1.40
+    // }, {
+    //     name: 'Sogou Explorer',
+    //     y: 0.84
+    // }, {
+    //     name: 'QQ',
+    //     y: 0.51
+    // }, {
+    //     name: 'Other',
+    //     y: 2.6
+    // }];
+    Highcharts.chart(id, {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Browser market shares in May, 2020',
+            align: 'left'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: data
+        }]
+    });
+}
