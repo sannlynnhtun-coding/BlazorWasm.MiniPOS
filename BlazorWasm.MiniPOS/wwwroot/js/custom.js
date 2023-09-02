9b76d3fad42d67792fc8e3823083c575869977b1
Mg Chit <chitlunh@gmail.com>
2023-09-02 21:56:53 +0630

    -

--- BlazorWasm.MiniPOS/wwwroot/js/custom.js
+++ BlazorWasm.MiniPOS/wwwroot/js/custom.js
@@ -575,3 +575,391 @@ window.barRaceChart = function () {
     });
 
 }
+
+window.columnRangeChart = function () {
+    Highcharts.chart('ColumnRangeChart', {
+
+        chart: {
+            type: 'column',
+            inverted: true
+        },
+
+        accessibility: {
+            description: 'Image description: A column range chart compares the monthly temperature variations throughout 2017 in Vik I Sogn, Norway. The chart is interactive and displays the temperature range for each month when hovering over the data. The temperature is measured in degrees Celsius on the X-axis and the months are plotted on the Y-axis. The lowest temperature is recorded in March at minus 10.2 Celsius. The lowest range of temperatures is found in December ranging from a low of minus 9 to a high of 8.6 Celsius. The highest temperature is found in July at 26.2 Celsius. July also has the highest range of temperatures from 6 to 26.2 Celsius. The broadest range of temperatures is found in May ranging from a low of minus 0.6 to a high of 23.1 Celsius.'
+        },
+
+        title: {
+            text: 'Temperature variation by month'
+        },
+
+        subtitle: {
+            text: 'Observed in Vik i Sogn, Norway, 2021 | ' +
+                'Source: <a href="https://www.vikjavev.no/ver/" target="_blank">Vikjavev</a>'
+        },
+
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+
+        yAxis: {
+            title: {
+                text: 'Temperature ( °C )'
+            }
+        },
+
+        tooltip: {
+            valueSuffix: '°C'
+        },
+
+        plotOptions: {
+            columnrange: {
+                borderRadius: '50%',
+                dataLabels: {
+                    enabled: true,
+                    format: '{y}°C'
+                }
+            }
+        },
+
+        legend: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Temperatures',
+            data: [
+                [-13.9, 5.2],
+                [-16.7, 10.6],
+                [-4.7, 11.6],
+                [-4.4, 16.8],
+                [-2.1, 27.2],
+                [5.9, 29.4],
+                [6.5, 29.1],
+                [4.7, 25.4],
+                [4.3, 21.6],
+                [-3.5, 15.1],
+                [-9.8, 12.5],
+                [-11.5, 8.4]
+            ]
+        }]
+
+    });
+
+}
+
+window.columnDrillDown = function () {
+    Highcharts.chart('ColumnDrillDown', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            align: 'left',
+            text: 'Browser market shares. January, 2022'
+        },
+        subtitle: {
+            align: 'left',
+            text: 'Click the columns to view versions. Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+        },
+        accessibility: {
+            announceNewData: {
+                enabled: true
+            }
+        },
+        xAxis: {
+            type: 'category'
+        },
+        yAxis: {
+            title: {
+                text: 'Total percent market share'
+            }
+
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            series: {
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.y:.1f}%'
+                }
+            }
+        },
+
+        tooltip: {
+            headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+            pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.2f}%</b> of total<br/>'
+        },
+
+        series: [
+            {
+                name: 'Browsers',
+                colorByPoint: true,
+                data: [
+                    {
+                        name: 'Chrome',
+                        y: 63.06,
+                        drilldown: 'Chrome'
+                    },
+                    {
+                        name: 'Safari',
+                        y: 19.84,
+                        drilldown: 'Safari'
+                    },
+                    {
+                        name: 'Firefox',
+                        y: 4.18,
+                        drilldown: 'Firefox'
+                    },
+                    {
+                        name: 'Edge',
+                        y: 4.12,
+                        drilldown: 'Edge'
+                    },
+                    {
+                        name: 'Opera',
+                        y: 2.33,
+                        drilldown: 'Opera'
+                    },
+                    {
+                        name: 'Internet Explorer',
+                        y: 0.45,
+                        drilldown: 'Internet Explorer'
+                    },
+                    {
+                        name: 'Other',
+                        y: 1.582,
+                        drilldown: null
+                    }
+                ]
+            }
+        ],
+        drilldown: {
+            breadcrumbs: {
+                position: {
+                    align: 'right'
+                }
+            },
+            series: [
+                {
+                    name: 'Chrome',
+                    id: 'Chrome',
+                    data: [
+                        [
+                            'v65.0',
+                            0.1
+                        ],
+                        [
+                            'v64.0',
+                            1.3
+                        ],
+                        [
+                            'v63.0',
+                            53.02
+                        ],
+                        [
+                            'v62.0',
+                            1.4
+                        ],
+                        [
+                            'v61.0',
+                            0.88
+                        ],
+                        [
+                            'v60.0',
+                            0.56
+                        ],
+                        [
+                            'v59.0',
+                            0.45
+                        ],
+                        [
+                            'v58.0',
+                            0.49
+                        ],
+                        [
+                            'v57.0',
+                            0.32
+                        ],
+                        [
+                            'v56.0',
+                            0.29
+                        ],
+                        [
+                            'v55.0',
+                            0.79
+                        ],
+                        [
+                            'v54.0',
+                            0.18
+                        ],
+                        [
+                            'v51.0',
+                            0.13
+                        ],
+                        [
+                            'v49.0',
+                            2.16
+                        ],
+                        [
+                            'v48.0',
+                            0.13
+                        ],
+                        [
+                            'v47.0',
+                            0.11
+                        ],
+                        [
+                            'v43.0',
+                            0.17
+                        ],
+                        [
+                            'v29.0',
+                            0.26
+                        ]
+                    ]
+                },
+                {
+                    name: 'Firefox',
+                    id: 'Firefox',
+                    data: [
+                        [
+                            'v58.0',
+                            1.02
+                        ],
+                        [
+                            'v57.0',
+                            7.36
+                        ],
+                        [
+                            'v56.0',
+                            0.35
+                        ],
+                        [
+                            'v55.0',
+                            0.11
+                        ],
+                        [
+                            'v54.0',
+                            0.1
+                        ],
+                        [
+                            'v52.0',
+                            0.95
+                        ],
+                        [
+                            'v51.0',
+                            0.15
+                        ],
+                        [
+                            'v50.0',
+                            0.1
+                        ],
+                        [
+                            'v48.0',
+                            0.31
+                        ],
+                        [
+                            'v47.0',
+                            0.12
+                        ]
+                    ]
+                },
+                {
+                    name: 'Internet Explorer',
+                    id: 'Internet Explorer',
+                    data: [
+                        [
+                            'v11.0',
+                            6.2
+                        ],
+                        [
+                            'v10.0',
+                            0.29
+                        ],
+                        [
+                            'v9.0',
+                            0.27
+                        ],
+                        [
+                            'v8.0',
+                            0.47
+                        ]
+                    ]
+                },
+                {
+                    name: 'Safari',
+                    id: 'Safari',
+                    data: [
+                        [
+                            'v11.0',
+                            3.39
+                        ],
+                        [
+                            'v10.1',
+                            0.96
+                        ],
+                        [
+                            'v10.0',
+                            0.36
+                        ],
+                        [
+                            'v9.1',
+                            0.54
+                        ],
+                        [
+                            'v9.0',
+                            0.13
+                        ],
+                        [
+                            'v5.1',
+                            0.2
+                        ]
+                    ]
+                },
+                {
+                    name: 'Edge',
+                    id: 'Edge',
+                    data: [
+                        [
+                            'v16',
+                            2.6
+                        ],
+                        [
+                            'v15',
+                            0.92
+                        ],
+                        [
+                            'v14',
+                            0.4
+                        ],
+                        [
+                            'v13',
+                            0.1
+                        ]
+                    ]
+                },
+                {
+                    name: 'Opera',
+                    id: 'Opera',
+                    data: [
+                        [
+                            'v50.0',
+                            0.96
+                        ],
+                        [
+                            'v49.0',
+                            0.82
+                        ],
+                        [
+                            'v12.1',
+                            0.14
+                        ]
+                    ]
+                }
+            ]
+        }
+    });
+}
